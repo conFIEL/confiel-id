6a2dc9b6427a46695d70c6ffbf7503025140566d
jjperezaguinaga <me@jjperezaguinaga.com>
2023-07-20 21:27:43 +0000

    feature: eFirma load completed to have in system

--- src/components/FIEL/FIELLoadButton.tsx
+++ src/components/FIEL/FIELLoadButton.tsx
@@ -1,88 +1,75 @@
 import { useContext, useEffect, useState } from "react";
 import { FIELStoreContext } from "./FIELContext";
-import { Credential as NodeCFDICredential, SignatureAlgorithm } from "@nodecfdi/credentials";
-import { KEYUTIL, RSAKey, hex2b64 } from 'jsrsasign';
+import {
+  Credential as NodeCFDICredential,
+  SignatureAlgorithm,
+} from "@nodecfdi/credentials";
+import { KEYUTIL, RSAKey, hex2b64 } from "jsrsasign";
 import { importPrivateKey } from "../../helpers/RSA";
 import { bufferToHex } from "../../helpers/buffers";
-import { Button, Input, Text } from "@chakra-ui/react";
-
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import { CircleIcon } from "../atoms/CircleIcon";
 
 type SignedData = {
   signedToken: string;
   validTo: string;
-}
+};
 
 export const FIELLoadButton = () => {
   const context = useContext(FIELStoreContext);
-  if (!context) throw new Error("FIELLoadButton must be used within a FIELStoreProvider");
+  if (!context)
+    throw new Error("FIELLoadButton must be used within a FIELStoreProvider");
 
+  const [isLoadingFIEL, setLoadingFiel] = useState(false);
   const [canLoadFIEL, setCanLoadFIEL] = useState(false);
-  const [tokenUUID, setTokenUUID] = useState("");
-  const [signedData, setSignedData] = useState<SignedData>(null);
+  const [cryptoRSAKey, setCryptoRSAkey] = useState<CryptoKey>(null);
 
   const [state] = context;
   const { privateKey, certificate, password } = state;
 
   const handleFIELLoad = async () => {
+    setLoadingFiel(true);
     const fiel = NodeCFDICredential.create(
       String(certificate),
       String(privateKey),
       password
     );
-    const eFirma = fiel.certificate();
 
-    if (!tokenUUID) {
-      console.log("No token found");
-      return;
-    }
-
-    const co = `${tokenUUID}|${eFirma.rfc()}|${eFirma.serialNumber().bytes()}`;
-
-    const rsaKey: RSAKey = KEYUTIL.getKeyFromEncryptedPKCS8PEM(fiel.privateKey().pem(), fiel.privateKey().passPhrase())
+    const rsaKey: RSAKey = KEYUTIL.getKeyFromEncryptedPKCS8PEM(
+      fiel.privateKey().pem(),
+      fiel.privateKey().passPhrase()
+    );
     const decryptedPEM = KEYUTIL.getPEM(rsaKey, "PKCS8PRV");
     const cryptoKey = await importPrivateKey(decryptedPEM);
-    const encoded = new TextEncoder().encode(co);
-    const signature = await window.crypto.subtle.sign(
-      "RSASSA-PKCS1-v1_5",
-      cryptoKey,
-      encoded
-    );
-    const signatureAsHex = bufferToHex(signature);
-    const digestSignatureAsB64 = hex2b64(signatureAsHex);
 
-    const digestAsHex = fiel.sign(co, SignatureAlgorithm.SHA1);
-    const digestAsB64 = hex2b64(digestAsHex)
-
-    console.log("Is native same as from eFirma", digestAsB64, digestSignatureAsB64, digestAsB64 === digestSignatureAsB64);
-
-    const lf = btoa(digestAsB64)
-    const token = btoa(btoa(co) + "#" + lf);
-
-    setSignedData({ signedToken: token, validTo: eFirma.validTo() })
-  }
-
-  const handleTokenUUIDChange = (e) => setTokenUUID( e.target.value );
+    setCryptoRSAkey(cryptoKey);
+    setLoadingFiel(false);
+  };
 
   useEffect(() => {
-    if (privateKey && certificate && (password?.length != 0)) {
+    if (privateKey && certificate && password?.length != 0) {
       setCanLoadFIEL(true);
     }
-    return () => setCanLoadFIEL(false)
-  }, [privateKey, certificate, password])
+    return () => setCanLoadFIEL(false);
+  }, [privateKey, certificate, password]);
 
-  return (!canLoadFIEL ?
-    <Text py="2" fontSize={'xs'}>Please upload all files and type password</Text> :
-    <div>
-      <Input placeholder="tokenUUID" type="text" onChange={handleTokenUUIDChange} value={tokenUUID} />
-      <Button onClick={handleFIELLoad}>Load FIEL</Button>
-      {
-        signedData &&
-        <>
-          <p>Signed Token: {signedData.signedToken}</p><br/>
-          <p>Valid To: {signedData.validTo}</p>
-        </>
-        
-      }
-    </div>
-  )
-}
+  return !canLoadFIEL ? (
+    <Text py="2" fontSize={"xs"}>
+      Please upload all files and type password
+    </Text>
+  ) : (
+    <>
+      <Button isLoading={isLoadingFIEL} mt="2" onClick={handleFIELLoad}>
+        Load FIEL
+      </Button>
+      <Flex justifyContent={"center"} alignItems={"center"} mt="2">
+        <CircleIcon color={cryptoRSAKey ? "green.500" : "red.500"} mr='2'/>
+        <Text fontSize={'xs'} fontFamily={"mono"}>
+          {cryptoRSAKey
+            ? "La e.FIRMA se ha cargado."
+            : "Sin e.FIRMA en el sistema"}
+        </Text>
+      </Flex>
+    </>
+  );
+};
